228425f44f385d0688d40cb63e8631c4880b91aa
eight9080 <eight9080@gmail.com>
2018-10-28 20:53:03 +0200

    order strings

--- CapitalizedStrings.js
+++ CapitalizedStrings.js
@@ -9,5 +9,29 @@ String.prototype.toJadenCase = function () {
   }
   );
 
-}
+};
 console.log("People  all".toJadenCase());
+
+function order(words){
+  var arrayWords = words.split(/\s+/g);
+  var result = [];
+  arrayWords.forEach(element => {
+    var val = element.match(/[0-9]+/g);
+    result[val]=element;
+  });
+  var resultString ="";
+  result.forEach(el => {
+    resultString+=el+" ";
+  });
+  return resultString.trim();
+}
+
+//order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est"
+console.log("Order: "+order("is2 Thi1s T4est 3a"));
+
+function order2(words){
+  
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+}    
